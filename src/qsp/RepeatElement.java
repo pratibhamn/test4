3fb7998e0fbb3d46154d4a14e757acfdc2608b7e
Pratibha <Pratibha@TYSS-4F-009>
2018-03-22 15:53:54 +0530

    2nd commit

--- src/qsp/RepeatElement.java
+++ src/qsp/RepeatElement.java
@@ -24,7 +24,7 @@ public class RepeatElement {
 	public static void main(String[] args) 
 	{
 		 RepeatElement repeat = new RepeatElement();
-	        int arr[] = {4, 2, 4, 5, 2, 3, 1};
+	        int arr[] = {4, 2, 4, 5, 2, 3, 1,0};
 	        int arr_size = arr.length;
 	        repeat.printRepeating(arr, arr_size);
 	}
